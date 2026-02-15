e9afea35e52c9cf8308f09485d7fbe1ced48659b
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-02-15 19:21:33 +0300

    update users_list_tile

--- lib/widgets/user_list_tile.dart
+++ lib/widgets/user_list_tile.dart
@@ -2,6 +2,7 @@ import 'package:chat_with_me/models/chat_user.dart';
 import 'package:chat_with_me/widgets/circle_avatar_with_status.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class UserListTile extends StatelessWidget {
   final ChatUser user;
@@ -18,6 +19,10 @@ class UserListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
+    final lastActiveDate = timeago.format(
+      user.lastActiveDate.toDate(),
+      locale: 'ru',
+    );
 
     return ListTile(
       minVerticalPadding: height * 0.02,
@@ -44,7 +49,7 @@ class UserListTile extends StatelessWidget {
               ],
             )
           : Text(
-              '${user.email}...',
+              lastActiveDate,
               style: TextStyle(
                 color: Colors.white70,
                 fontWeight: FontWeight.w500,
